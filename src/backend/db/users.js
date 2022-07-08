336e759f78ef592b1d9ecda7b88bb49b2adc02d1
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-07-08 23:38:01 +0530

    refactor:update title & user data

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -15,4 +15,13 @@ export const users = [
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
+   {
+    _id: uuid(),
+    firstName: "Shivani",
+    lastName: "B",
+    username: "shivani",
+    password: "shivani@12",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
 ];
