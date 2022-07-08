595fd67a76ca6518f3b52a8c11941eaace9be0e5
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-07-08 23:45:19 +0530

    refactor:provide store to render app & update to react18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import * as ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/app/store";
 import { makeServer } from "./server";
-
+import { BrowserRouter as Router } from "react-router-dom";
 // Call make Server
 makeServer();
-
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>
 );
