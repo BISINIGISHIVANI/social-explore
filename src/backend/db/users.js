c0d92ba325116d829cde4bf639afd5cf0d458f6b
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-07-11 22:47:17 +0530

    feat:add images ,users data to backend

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -4,23 +4,67 @@ import { formatDate } from "../utils/authUtils";
  * User Database can be added here.
  * You can add default users of your wish with different attributes
  * */
-
+import { charle,glif,mlane,zxocde,remy } from "../../assets/avatar";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    username: "adarshbalika",
-    password: "adarshBalika123",
+    firstName: "Richard",
+    lastName: "Tucker",
+    username: "Richard",
+    password: "us@142",
+    avatar:{glif},
+    bio:"",
     createdAt: formatDate(),
     updatedAt: formatDate(),
-  },
-   {
+  }
+  ,{
+    _id: uuid(),
+    firstName: "Jose",
+    lastName: "Bowman",
+    username: "",
+    password: "charlie23",
+    avatar:{mlane},
+    bio:"",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },{
+    _id: uuid(),
+    firstName: "Roy",
+    lastName: "Nelson",
+    username: "",
+    password: "nelson45@",
+    avatar:{zxocde},
+    bio:"",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },{
+    _id: uuid(),
+    firstName: "Laura",
+    lastName: "Dixon",
+    username: "",
+    password: "4Kpng@",
+    avatar:{charle},
+    bio:"",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },{
+    _id: uuid(),
+    firstName: "Henry",
+    lastName: "Warren",
+    username: "",
+    password: "henry56",
+    avatar:{remy},
+    bio:"",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  }, {
     _id: uuid(),
     firstName: "Shivani",
     lastName: "B",
     username: "shivani",
     password: "shivani@12",
+    avatar:{glif},
+    bio:"",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
