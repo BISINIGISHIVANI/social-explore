9f1c04051d27d48b395cfb02477154a999e2212b
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-07-22 16:23:32 +0530

    refactor:export posts,users in home page

--- src/pages/homePage/home.js
+++ src/pages/homePage/home.js
@@ -1,7 +1,35 @@
 import "./home.css";
 import { Navbar } from "../../component/navbar/navbar";
 import { Sidebar } from "../../component/sidebar/sidebar";
+import { getPosts } from "../../redux/asyncThunk/postThunk";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { PostCard } from "../../component/postcard/postcard";
+import { PostModal } from "../../component/postModal/postModal";
+import { SuggestUser } from "../../component/userSidebar/userSidebar";
+import { EditPostModal } from "../../component/editPostModal/editPostModal";
+import { getAllUsers } from "../../redux/asyncThunk/userThunk";
 export const Home = () => {
+  const dispatch = useDispatch();
+  const { posts, status } = useSelector((state) => state.posts);
+  const {
+    user: { following, username }
+  } = useSelector((state) => state.auth);
+  const { editPostModal } = useSelector((state) => state.modal);
+  const reversePosts = [...posts].reverse();
+  const followingUsersArr = [...following].map((user) => user.username);
+  const followingPosts = posts.filter((post) =>
+    followingUsersArr.includes(post.username)
+  );
+  const getFollowingPosts = reversePosts
+    .filter((post) => post.username === username)
+    .concat(followingPosts);
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(getPosts());
+      dispatch(getAllUsers());
+    }
+  }, [status, dispatch]);
   return (
     <div>
       <Navbar />
@@ -10,11 +38,17 @@ export const Home = () => {
           <Sidebar />
         </div>
         <section className="flex-row main-content">
-          <section className="bd-sm content"></section>
-          <aside className="bd-left padding-edges aside">
-            <img src="" alt="" />
-            <h3>Mahabaratham</h3>
-            <button>Follow</button>
+          <section className="bd-sm content flex-col gap">
+            <h2>hey,welcome back</h2>
+            {editPostModal ? <EditPostModal /> : <PostModal />}
+            <div className="flex-col gap">
+              {getFollowingPosts.map((postData) => (
+                <PostCard key={postData._id} {...postData} />
+              ))}
+            </div>
+          </section>
+          <aside className="padding-edges aside cursor-pointer">
+            <SuggestUser />
           </aside>
         </section>
       </div>
