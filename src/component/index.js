4983863afb6d9cf96a6191bc52e42e27b3110887
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-07-27 21:36:25 +0530

    refactor:update darkmode content color,add post modal

---
+++ src/component/index.js
@@ -0,0 +1,6 @@
+import { PostCard } from "./postCard/postCard";
+import { Navbar } from "./navbar/navbar";
+import { Sidebar } from "./sidebar/sidebar";
+import { AddPost } from "./addPost/addPost";
+import { PostModal } from "./postModal/postModal";
+export {Navbar,Sidebar,PostCard,AddPost,PostModal}
