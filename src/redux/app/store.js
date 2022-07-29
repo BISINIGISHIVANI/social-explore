25d1981a13dddd5912b062c0df92109c5ee1b030
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-07-29 18:57:20 +0530

    feat:create users thunk ,userslice

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -3,11 +3,16 @@ import { authReducer } from "../features/authSlice";
 import { postsReducer } from "../features/postSlice";
 import { modalReducer } from "../features/modalSlice";
 import { usersReducer } from "../features/userSlice";
+import { editProfileReducer } from "../features/profileSlice";
+import { userProfileReducer } from "../features/singleUserProfileSlice";
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     posts: postsReducer,
     modal: modalReducer,
-    users: usersReducer
+    users: usersReducer,
+    editProfile:editProfileReducer,
+    userProfile:userProfileReducer
   }
 });
--- src/redux/features/userSlice.js
+++ src/redux/features/userSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getAllUsers } from "../asyncThunk/userThunk";
+import { editUser, followUser, getAllUsers,unFollowUser } from "../asyncThunk/userThunk";
 const initialState = {
   users: [],
+  userProfile:{},
   status: "idle",
   loading: false,
   error: null
@@ -19,9 +20,35 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = "";
       state.status = "resolved";
-      state.users = action.payload.users;
+      state.users = action.payload;
     },
     [getAllUsers.rejected]: (state, action) => {
+      state.loading = false;
+      state.status = "rejected";
+      state.error = action.payload.error;
+    },
+    [followUser.fulfilled]:(state,action)=>{
+      const { followUser } = action.payload;
+      state.users = [...state.users].map((user) =>
+        user._id === followUser._id ? followUser : user
+      );
+    },
+    [followUser.rejected]:(state,action)=>{
+      state.error=action.payload;
+    },
+    [unFollowUser.fulfilled]:(state,action)=>{
+      const { followUser } = action.payload;
+      state.users = [...state.users].map((user) =>
+        user._id === followUser._id ? followUser : user
+      );
+    },
+    [editUser.fulfilled]:(state,action)=>{
+      state.loading = false;
+      state.error = "";
+      state.status = "resolved";
+      state.userProfile=action.payload;
+    },
+    [editUser.rejected]:(state,action)=>{
       state.loading = false;
       state.status = "rejected";
       state.error = action.payload.error;
